4f48cab70ff0c73babad0191749373a8c9f2b26f
Rakesh-sensen <98682512+Rakesh-sensen@users.noreply.github.com>
2022-04-11 20:12:49 +0530

    Update vl.py

--- vl.py
+++ vl.py
@@ -18,7 +18,7 @@ with col2:
 with col1:
     pass
 with  col3:
-     i=Image.open('roc.jpg')
+     i=Image.open('ro.jpg')
      st.set_option('deprecation.showPyplotGlobalUse', False)                          #image projection
      st.image(i,use_column_width=None)
 
